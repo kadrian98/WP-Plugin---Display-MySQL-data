9f64200f4955d3d2cd8a2b1acbcf787d395e6396
Adrian <k_adrian@wp.pl>
2024-02-01 20:31:15 +0100

    improve apperance

--- includes/DisplayDataFromDB.php
+++ includes/DisplayDataFromDB.php
@@ -26,12 +26,18 @@ public function displayData(){
     $result = $this->fetchData();
     
     if ($result->num_rows > 0) {
+        echo '<table class="data-table">';
+        echo '<tr><th>ID</th><th>Nazwa Klienta</th><th>Email Klienta</th><th>Wiadomość</th><th>Akcje</th></tr>'; // Nagłówki tabeli
         while($row = $result->fetch_assoc()) {
-            echo '<div id="row-' . $row["id"] . '">';
-            echo $row["id"] . " - " . $row["nazwa_klienta"];
-            echo ' <button class="btn" data-id="' . $row["id"] . '">Delete</button>';
-            echo '</div><br>';
+            echo '<tr id="row-' . $row["id"] . '">';
+            echo '<td>' . $row["id"] . '</td>';
+            echo '<td>' . $row["nazwa_klienta"] . '</td>';
+            echo '<td>' . $row["email_klienta"] . '</td>';
+            echo '<td>' . $row["wiadomosc"] . '</td>';
+            echo '<td><button class="btn" data-id="' . $row["id"] . '">Delete</button></td>';
+            echo '</tr>';
         }
+        echo '</table>';
     } else {
         echo "0 results";
     }
